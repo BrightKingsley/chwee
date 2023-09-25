974772183ecf65e5c9fe0b15e8919c5a2440eb59
Bright Kingsley <briggskvngzz@gmail.com>
2023-09-26 00:56:30 +0100

    add '@ts-nocheck' to top of client-only-portal component

--- components/ClientOnlyPortal/ClientOnlyPortal.tsx
+++ components/ClientOnlyPortal/ClientOnlyPortal.tsx
@@ -1,17 +1,24 @@
-"use client"
+//@ts-nocheck
+"use client";
 
 import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
-export default function ClientOnlyPortal({children,selector}:{children:React.ReactNode; selector:any}) {
-const ref = useRef()
-const [mounted, setMounted] = useState(false)
+export default function ClientOnlyPortal({
+  children,
+  selector,
+}: {
+  children: React.ReactNode;
+  selector: any;
+}) {
+  const ref = useRef();
+  const [mounted, setMounted] = useState(false);
 
-useEffect(()=>{
-ref.current = document.querySelector(selector)
+  useEffect(() => {
+    ref.current = document.querySelector(selector);
 
-setMounted(true)
-},[selector])
+    setMounted(true);
+  }, [selector]);
 
-  return  mounted && ref.current? createPortal(children, ref.current) : null
+  return mounted && ref.current ? createPortal(children, ref.current) : null;
 }
