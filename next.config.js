72905d00021c1d65354ac5d1b76ed0dc68a5f095
Bright Kingsley <110265893+BrightKingsley@users.noreply.github.com>
2023-09-28 19:39:30 +0100

    Update next.config.js: removed server property

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com","utfs.io"],
   },
-  server: { host: "0.0.0.0" },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose", "@typegoose/typegoose"],
