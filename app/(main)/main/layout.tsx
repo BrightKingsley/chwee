a83e3f22e31720276cd37bd9410bb3cd1b64d6d3
Bright Kingsley <briggskvngzz@gmail.com>
2023-10-28 17:37:17 +0100

    Work on Landing Page - (Font and Metadata). Minor bug fixes. This Commit is Before changing Message structure, changing from an array to an object/map

--- app/(main)/main/layout.tsx
+++ app/(main)/main/layout.tsx
@@ -19,59 +19,10 @@ import "swiper/css";
 import type { Metadata } from "next";
 import { Rat } from "@/app/components/server";
 const keywords = [
-  "bill while in chat",
-  "send, recive money",
+  "in-chat billing",
+  "send, receive money",
   "real-time communication",
-  // "gaming event solution",
-  // "community gaming",
-  // "online gaming event",
-  // "hassle-free raffle",
-  // "gaming event management",
-  // "DRAWR platform",
-  // "raffle logistics",
-  // "online gaming community",
-  // "customer engagement",
-  // "seamless event management",
-  // "captivating gaming experiences",
-  // "gaming event organizers",
-  // "powerful gaming platform",
-  // "organize raffle draw online",
-  // "effortless gaming events",
-  // "interactive raffle experience",
-  // "enhance customer experience",
-  // "digital gaming events",
-  // "online event ticketing",
-  // "online prize draws",
-  // "exciting raffle campaigns",
-  // "raffle software solution",
 ];
-
-// export const metadata: Metadata = {
-//   metadataBase: new URL("https://drawr.vercel.app"),
-//   title:
-//     "DRAWR: Simplifying Your Online Raffle Draw - The Ultimate Gaming Event Solution",
-//   description: description,
-//   icons: { apple: "/apple-touch-icon.png", icon: "" },
-//   viewport: { width: "device-width", initialScale: 1 },
-//   keywords: keywords,
-//   openGraph: {
-//     type: "website",
-//     url: "/",
-//     title:
-//       "DRAWR: Simplifying Your Online Raffle Draw - The Ultimate Gaming Event Solution",
-//     description: description,
-//     siteName: "Drawr",
-//     locale: "en_UK",
-//   },
-//   twitter: {
-//     site: "/",
-//     title:
-//       "DRAWR: Simplifying Your Online Raffle Draw - The Ultimate Gaming Event Solution",
-//     creator: "@drawr",
-//     card: "summary_large_image",
-//     description: description,
-//   },
-// };
 
 export default function RootLayout({
   children,
