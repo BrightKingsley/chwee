0adc1000928d0798defbefc78332a5ffc227cf89
Bright Kingsley <briggskvngzz@gmail.com>
2024-04-05 20:50:17 +0100

    fix: client exception error

--- context/Theme/themeContext.tsx
+++ context/Theme/themeContext.tsx
@@ -1,21 +1,17 @@
-"use client";
+'use client';
 import {
   SpinnerStyleTypes,
   TextareaStylesType,
   ThemeProvider,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react';
 import type {
   ButtonStyleTypes,
   IconButtonStyleTypes,
   InputStylesType,
   SwitchButtonStylesType,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react';
 
-export function ThemeContextProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export function ThemeContextProvider({children}: {children: any}) {
   const customTheme: {
     button: ButtonStyleTypes;
     iconButton: IconButtonStyleTypes;
@@ -27,31 +23,31 @@ export function ThemeContextProvider({
     button: {
       defaultProps: {
         // @ts-ignore
-        color: "deep-orange",
+        color: 'deep-orange',
         ripple: true,
-        variant: "gradient",
-        className: "!font-poppins",
+        variant: 'gradient',
+        className: '!font-poppins',
       },
       styles: {
         variants: {
           filled: {
-            "deep-orange": {
-              background: "bg-primary",
-              color: "text-white",
-              shadow: "shadow-md shadow-primary/10",
-              hover: "hover:shadow-lg hover:primary/20",
-              focus: "focus:opacity-[0.85] focus:shadow-none",
-              active: "active:opacity-[0.85] active:shadow-none",
+            'deep-orange': {
+              background: 'bg-primary',
+              color: 'text-white',
+              shadow: 'shadow-md shadow-primary/10',
+              hover: 'hover:shadow-lg hover:primary/20',
+              focus: 'focus:opacity-[0.85] focus:shadow-none',
+              active: 'active:opacity-[0.85] active:shadow-none',
             },
           },
           outlined: {
-            "deep-orange": {
-              background: "outline-primary",
-              color: "text-primary",
-              shadow: "shadow-none",
-              hover: "",
-              focus: "",
-              active: "",
+            'deep-orange': {
+              background: 'outline-primary',
+              color: 'text-primary',
+              shadow: 'shadow-none',
+              hover: '',
+              focus: '',
+              active: '',
             },
           },
           gradient: {},
@@ -61,22 +57,22 @@ export function ThemeContextProvider({
     iconButton: {
       defaultProps: {
         ripple: true,
-        variant: "text",
-        className: "flex items-center justify-center",
+        variant: 'text',
+        className: 'flex items-center justify-center',
         // @ts-ignore
-        color: "deep-orange",
+        color: 'deep-orange',
         //TODO size, fullwidth
       },
       styles: {
         variants: {
           filled: {
-            "deep-orange": {
-              background: "bg-primary",
-              color: "text-white",
-              shadow: "shadow-md shadow-primary/10",
-              hover: "hover:shadow-lg hover:primary/20",
-              focus: "focus:opacity-[0.85] focus:shadow-none",
-              active: "active:opacity-[0.85] active:shadow-none",
+            'deep-orange': {
+              background: 'bg-primary',
+              color: 'text-white',
+              shadow: 'shadow-md shadow-primary/10',
+              hover: 'hover:shadow-lg hover:primary/20',
+              focus: 'focus:opacity-[0.85] focus:shadow-none',
+              active: 'active:opacity-[0.85] active:shadow-none',
             },
           },
           gradient: {},
@@ -87,25 +83,25 @@ export function ThemeContextProvider({
 
     input: {
       // @ts-ignore
-      defaultProps: { variant: "outlined", color: "deep-orange" },
+      defaultProps: {variant: 'outlined', color: 'deep-orange'},
     },
 
     switch: {
       defaultProps: {
         // @ts-ignore
-        color: "deep-orange",
+        color: 'deep-orange',
       },
     },
 
     spinner: {
       defaultProps: {
         // @ts-ignore
-        color: "deep-orange",
+        color: 'deep-orange',
       },
     },
     textarea: {
       // @ts-ignore
-      defaultProps: { variant: "outlined", color: "deep-orange" },
+      defaultProps: {variant: 'outlined', color: 'deep-orange'},
     },
   };
 
